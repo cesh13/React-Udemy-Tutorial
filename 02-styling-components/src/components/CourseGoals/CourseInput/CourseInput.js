3e80a28e427b5fc4ff1a0bcb537c322a4ac9f7d6
Cesar Molina <cesh_m@hotmail.com>
2021-07-20 22:44:13 -0600

    80 dynamic styles with css modules

--- 02-styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 02-styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
-import styled from 'styled-components'
+import styles from './CourseInput.module.css';
+// import styled from 'styled-components'
 
-const FormControl = styled.div`
-& {
-  margin: 0.5rem 0;
-}
+// const FormControl = styled.div`
+// & {
+//   margin: 0.5rem 0;
+// }
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${props => props.valid ? 'black' : 'red'};
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color: ${props => props.valid ? 'black' : 'red'};
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => props.valid ? '#ccc' : 'red'};
-  background: ${props => props.valid ? 'transparent' : '#ffd7d7'};
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
-`;
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props => props.valid ? '#ccc' : 'red'};
+//   background: ${props => props.valid ? 'transparent' : '#ffd7d7'};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
+// `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -50,10 +50,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl valid={isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
